818493fc68bc30971cc35f602d6540ff3595a221
darkmift@gmail.com <darkmift@gmail.com>
2018-07-15 20:32:13 +0300

    its still crap but less so.

--- main.js
+++ main.js
@@ -28,20 +28,62 @@ function makeUser(int) {
                 cl('clicked del!')
                 $('#' + userInstance.login.uuid).remove()
             });
+
             //remove empty notice
             if ($('#nousersMsg').length) $('#nousersMsg').remove();
+
             //grab buttons from this instance user.html for assignemnt usage
             $(outputHtml).appendTo($('#userList'));
+
             //build btn functionality
             // cl($(outputHtml).find('.btn-danger'));
+
+            //delete contact
             $('.btn-danger').click((e) => {
-                cl($(e.target).closest('.userContainer'));
+                $(e.target).closest('.userContainer').remove();
             });
 
+            //edit contact
             $('.btn-edit').click((e) => {
-                cl($(e.target).closest('.userContainer'));
+                //grab the contact parent of this clicked button 
+                var container = $(e.target).closest('.userContainer');
+                //define the spans with info to edit
+                spanList = container.find('span');
+                //convert all span to input
+                spanList.each(function(index, element) {
+                    var content = $(element).text();
+                    var input = $('<input>', {
+                        value: content,
+                        class: "editInput"
+                    })
+                    cl($(element));
+                    $(element).replaceWith(input);
+                });
+                //make save button
+                //find btn group of edit button and append a save button
+                var btnGroup = $(e.target).closest('.btn-group');
+                $(btnGroup).find('button.btn-success').length !== 0 ? cl('it does') : btnGroup.append(
+                    $('<button>', {
+                        text: "save",
+                        class: "btn btn-success",
+                        click: (e) => {
+                            cl('clickedsuccess!');
+                            spanList = container.find('input');
+                            spanList.each(function(index, element) {
+                                cl(element, index);
+                                var content = $(element).val();
+                                var spanEdited = $('<span>', {
+                                    text: content,
+                                })
+                                $(element).replaceWith(spanEdited);
+                            });
+                            e.target.remove();
+                        }
+                    })
+                )
             });
-        })
+            //ask why its cl more than once...meanwhile ternary checking if hasclass then do not add anymore
+        });
     });
 }
 
@@ -49,3 +91,25 @@ function makeUser(int) {
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
+
+
+
+
+// $('#' + user.login.uuid + " .btn-group").append(
+//     $('<button>', {
+//         text: "save",
+//         class: "btn btn-success",
+//         click: (e) => {
+//             cl('clickedsuccess!');
+//             $('#' + user.login.uuid + ' input').each(function(index, element) {
+//                 cl(element, index);
+//                 var content = $(element).val();
+//                 var spanEdited = $('<span>', {
+//                     text: content,
+//                 })
+//                 $(element).replaceWith(spanEdited);
+//             });
+//             e.target.remove();
+//         }
+//     })
+// )
